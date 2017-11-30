10d4d947b6a3c1ed9a88e41f4a0da06352eb2a34
Ravi kumar <kumar.ravi@hotmail.co.uk>
2017-11-30 19:24:21 +0000

    adding in js script for navigation responsiveness

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+
+<script src="https://code.jquery.com/jquery-3.2.1.js></script>
+	  <script> type="text/javascript">
+	      $(document).ready(function(){
+		     $('.menu').click(function(){
+			    $('ul').toggleClass('active');
+			 })
+		  })
+		  </script>
