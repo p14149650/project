534ea2479e81ffa1d3ff504f1211157f1c178534
Ravi kumar <kumar.ravi@hotmail.co.uk>
2017-12-07 19:54:00 +0000

    remodified map api style

--- js/scriptsmaps.js
+++ js/scriptsmaps.js
@@ -5,7 +5,7 @@ function initMap() {
   let leicester = {lat: 52.6333, lng: -1.1333};
   let dmu = {lat: 52.629311, lng: -1.137836};
   let pahousing = {lat: 52.6295, lng: -1.124049};
-  let mapDemo = document.getElementById("map");
+  let mapDemo = document.getElementById("mymap");
 
   // this sets the default location for when the map is first loaded
   let map = new google.maps.Map(mapDemo, {
